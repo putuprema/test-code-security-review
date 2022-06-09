ef3b85976ea1b14da00826d9ee01bda75c56f316
putuprema <iputupremaananda@gmail.com>
2022-06-09 14:45:52 +0700

    initial commit

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace TestCodeSecurityReview.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string DisplayName { get; set; }
+    public string BankAccountNumber { get; set; }
+    public string SocialSecurityNumber { get; set; }
+}
